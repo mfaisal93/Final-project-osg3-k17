0229013ff728c72f50f52c99ae8de8f068e46f30
icuL <faisaru51@gmail.com>
2019-03-18 01:36:06 +0700

    issues4

---
+++ app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.osg3.finalprojectk17.finalprojectk17osg3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
