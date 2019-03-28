9ac5afa5fbbc57a449577fbc42718f7daa8bacff
fikrirazzaq <razzaq@recharge.id>
2019-03-28 13:40:39 +0700

    Task US2 done.

--- app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/DomainListActivity.java
+++ app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/DomainListActivity.java
@@ -1,26 +1,17 @@
 package com.osg3.finalprojectk17.finalprojectk17osg3;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Toast;
-
 import com.osg3.finalprojectk17.finalprojectk17osg3.adapter.DomainAdapter;
-import com.osg3.finalprojectk17.finalprojectk17osg3.api.APIClient;
 import com.osg3.finalprojectk17.finalprojectk17osg3.model.Domain;
-import com.osg3.finalprojectk17.finalprojectk17osg3.model.DomainSearch;
 import com.osg3.finalprojectk17.finalprojectk17osg3.utils.RecyclerItemClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 import static com.osg3.finalprojectk17.finalprojectk17osg3.MainActivity.EXTRA_KEYWORD;
 
@@ -41,7 +32,6 @@ public class DomainListActivity extends AppCompatActivity {
 	}
 
 	private void setUpDomainList() {
-		rcvDomain = findViewById(R.id.rcv_domain);
 		linearLayoutManager = new LinearLayoutManager(DomainListActivity.this);
 		rcvDomain.setLayoutManager(linearLayoutManager);
 		domainAdapter = new DomainAdapter(DomainListActivity.this, getDomainData());
@@ -71,8 +61,9 @@ public class DomainListActivity extends AppCompatActivity {
 	 * @return Data-data domain hasil pencarian
 	 */
 	private List<Domain> getDomainData() {
-        final List<Domain> searchResult = new ArrayList<>();
+		List<Domain> searchResult = new ArrayList<>();
 		String searchKeyword = getIntent().getStringExtra(EXTRA_KEYWORD);
+
 		//TODO: isi 'searchResult'
 
 		return searchResult;
