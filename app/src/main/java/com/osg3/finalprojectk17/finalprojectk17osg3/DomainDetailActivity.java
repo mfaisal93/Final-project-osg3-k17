be87338cbeaa2eda650fe17682f108c0762e3b69
fikrirazzaq <razzaq@recharge.id>
2019-03-28 13:40:39 +0700

    Task US2 done.

---
+++ app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/DomainDetailActivity.java
@@ -0,0 +1,6 @@
+package com.osg3.finalprojectk17.finalprojectk17osg3;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class DomainDetailActivity {
+}
---
+++ app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/DomainListActivity.java
@@ -0,0 +1,71 @@
+package com.osg3.finalprojectk17.finalprojectk17osg3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import com.osg3.finalprojectk17.finalprojectk17osg3.adapter.DomainAdapter;
+import com.osg3.finalprojectk17.finalprojectk17osg3.model.Domain;
+import com.osg3.finalprojectk17.finalprojectk17osg3.utils.RecyclerItemClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.osg3.finalprojectk17.finalprojectk17osg3.MainActivity.EXTRA_KEYWORD;
+
+public class DomainListActivity extends AppCompatActivity {
+
+	public static final String EXTRA_DOMAIN = "extra_domain";
+
+	private DomainAdapter domainAdapter;
+	private LinearLayoutManager linearLayoutManager;
+	private RecyclerView rcvDomain;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_domain_list);
+
+		setUpDomainList();
+	}
+
+	private void setUpDomainList() {
+		linearLayoutManager = new LinearLayoutManager(DomainListActivity.this);
+		rcvDomain.setLayoutManager(linearLayoutManager);
+		domainAdapter = new DomainAdapter(DomainListActivity.this, getDomainData());
+		addTouchEvent();
+	}
+
+	private void addTouchEvent() {
+		rcvDomain.addOnItemTouchListener(
+				new RecyclerItemClickListener(DomainListActivity.this, rcvDomain,
+						new RecyclerItemClickListener.OnItemClickListener() {
+							@Override
+							public void onItemClick(View view, int position) {
+								Intent intent = new Intent(DomainListActivity.this, DomainDetailActivity.class);
+								intent.putExtra(EXTRA_DOMAIN, domainAdapter.getDomainList().get(position));
+								startActivity(intent);
+							}
+
+							@Override
+							public void onLongItemClick(View view, int position) {
+
+							}
+						}));
+	}
+
+	/**
+	 *
+	 * @return Data-data domain hasil pencarian
+	 */
+	private List<Domain> getDomainData() {
+		List<Domain> searchResult = new ArrayList<>();
+		String searchKeyword = getIntent().getStringExtra(EXTRA_KEYWORD);
+
+		//TODO: isi 'searchResult'
+
+		return searchResult;
+	}
+}
