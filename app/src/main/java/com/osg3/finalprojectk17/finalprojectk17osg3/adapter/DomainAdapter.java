40dd40a9089beb4bc15e6e4ab5b85dd02a3e4b68
Irfan Erfolgi <erfolgi827@gmail.com>
2019-03-28 22:31:15 +0700

    Model and Network

--- app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/adapter/DomainAdapter.java
+++ app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/adapter/DomainAdapter.java
@@ -1,7 +1,6 @@
 package com.osg3.finalprojectk17.finalprojectk17osg3.adapter;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -10,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
-import com.osg3.finalprojectk17.finalprojectk17osg3.DomainDetailActivity;
 import com.osg3.finalprojectk17.finalprojectk17osg3.R;
 import com.osg3.finalprojectk17.finalprojectk17osg3.model.Domain;
 
