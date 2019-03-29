c6b91ee2c6d2956ea3997baa1350b907342003c4
fikrirazzaq <razzaq@recharge.id>
2019-03-29 09:58:59 +0700

    Init ViewModel in DomainListActivity. Add keyword param in getListDomain().

--- app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/DomainListActivity.java
+++ app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/DomainListActivity.java
@@ -5,37 +5,58 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import com.osg3.finalprojectk17.finalprojectk17osg3.adapter.DomainAdapter;
 import com.osg3.finalprojectk17.finalprojectk17osg3.model.Domain;
+import com.osg3.finalprojectk17.finalprojectk17osg3.navigator.DomainNavigator;
 import com.osg3.finalprojectk17.finalprojectk17osg3.utils.RecyclerItemClickListener;
+import com.osg3.finalprojectk17.finalprojectk17osg3.viewmodel.DomainViewModel;
+import com.osg3.finalprojectk17.finalprojectk17osg3.viewmodel.ViewModelInjector;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.osg3.finalprojectk17.finalprojectk17osg3.MainActivity.EXTRA_KEYWORD;
 
-public class DomainListActivity extends AppCompatActivity {
+public class DomainListActivity extends AppCompatActivity implements DomainNavigator {
 
+	private static final String TAG = DomainListActivity.class.getSimpleName();
 	public static final String EXTRA_DOMAIN = "extra_domain";
+
+	private DomainViewModel domainViewModel;
 
 	private DomainAdapter domainAdapter;
 	private LinearLayoutManager linearLayoutManager;
 	private RecyclerView rcvDomain;
+
+	private List<Domain> domainList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_domain_list);
 
+		getDomainData();
 		setUpDomainList();
+	}
+
+	@Override
+	public void loadListDomain(List<Domain> list) {
+		domainAdapter.setDomainList(list);
+	}
+
+	@Override
+	public void errorLoadListDomain(String message) {
+		Log.e(TAG, "errorLoadListDomain: " + message);
 	}
 
 	private void setUpDomainList() {
 		rcvDomain = findViewById(R.id.rcv_domain);
 		linearLayoutManager = new LinearLayoutManager(DomainListActivity.this);
 		rcvDomain.setLayoutManager(linearLayoutManager);
-		domainAdapter = new DomainAdapter(DomainListActivity.this, getDomainData());
+		domainAdapter = new DomainAdapter(DomainListActivity.this, domainList);
+		rcvDomain.setAdapter(domainAdapter);
 		addTouchEvent();
 	}
 
@@ -57,16 +78,13 @@ public class DomainListActivity extends AppCompatActivity {
 						}));
 	}
 
-	/**
-	 *
-	 * @return Data-data domain hasil pencarian
-	 */
-	private List<Domain> getDomainData() {
-		List<Domain> searchResult = new ArrayList<>();
+
+	private void getDomainData() {
 		String searchKeyword = getIntent().getStringExtra(EXTRA_KEYWORD);
+		domainList = new ArrayList<>();
 
-		//TODO: isi 'searchResult'
-
-		return searchResult;
+		domainViewModel = new DomainViewModel(this, ViewModelInjector.provideDomainRepository(this));
+		domainViewModel.setNavigator(this);
+		domainViewModel.getListDomain(searchKeyword);
 	}
 }
--- app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/data/remote/DomainRemoteDataSource.java
+++ app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/data/remote/DomainRemoteDataSource.java
@@ -5,8 +5,8 @@ import com.osg3.finalprojectk17.finalprojectk17osg3.data.DomainDataSource;
 public class DomainRemoteDataSource implements DomainDataSource {
 
 	@Override
-	public void getListDomain(GetDomainCallback callback) {
-
+	public void getListDomain(GetDomainCallback callback, String keyword) {
 		//TODO: Tambah kode buat response call
+
 	}
 }
