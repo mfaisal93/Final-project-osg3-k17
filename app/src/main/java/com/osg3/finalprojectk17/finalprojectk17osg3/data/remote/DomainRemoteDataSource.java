ea9166f927086576e9828738a9af89d879683ecc
fikrirazzaq <razzaq@recharge.id>
2019-03-29 09:36:27 +0700

    Task US1 (ViewModel) done. Create ViewModel, Injector, and Repository Pattern.

---
+++ app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/data/remote/DomainRemoteDataSource.java
@@ -0,0 +1,12 @@
+package com.osg3.finalprojectk17.finalprojectk17osg3.data.remote;
+
+import com.osg3.finalprojectk17.finalprojectk17osg3.data.DomainDataSource;
+
+public class DomainRemoteDataSource implements DomainDataSource {
+
+	@Override
+	public void getListDomain(GetDomainCallback callback) {
+
+		//TODO: Tambah kode buat response call
+	}
+}
