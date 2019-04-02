43f161b8bab93881d63dd490a74227b417a3d32b
fikrirazzaq <razzaq@recharge.id>
2019-04-02 16:53:42 +0700

    Fix intent extra key & Add Detail Activity to Manifest.

--- app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/DomainDetailActivity.java
+++ app/src/main/java/com/osg3/finalprojectk17/finalprojectk17osg3/DomainDetailActivity.java
@@ -6,9 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import com.osg3.finalprojectk17.finalprojectk17osg3.model.Domain;
 
-public class DomainDetailActivity extends AppCompatActivity{
+import static com.osg3.finalprojectk17.finalprojectk17osg3.DomainListActivity.EXTRA_DOMAIN;
+
+public class DomainDetailActivity extends AppCompatActivity {
 	String isDead, resolvable;
 	TextView tv_domain, tv_expiryDate, tv_createDate, tv_updatedate, tv_isDead, tv_a, tv_txt, tv_country, tv_ns, tv_resolvable;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,10 +27,10 @@ public class DomainDetailActivity extends AppCompatActivity{
 		tv_txt = (TextView) findViewById(R.id.tv_txt);
 		tv_country = (TextView) findViewById(R.id.tv_country);
 		tv_ns = (TextView) findViewById(R.id.tv_ns);
-		tv_resolvable= (TextView) findViewById(R.id.tv_resolvable);
+		tv_resolvable = (TextView) findViewById(R.id.tv_resolvable);
 
 		Intent intent = getIntent();
-		Domain domain = intent.getParcelableExtra("DomainDetail");
+		Domain domain = intent.getParcelableExtra(EXTRA_DOMAIN);
 		if (domain.isDead)
 			isDead = "Aktif";
 		else
@@ -41,13 +44,13 @@ public class DomainDetailActivity extends AppCompatActivity{
 		tv_createDate.setText(domain.createDate);
 		tv_updatedate.setText(domain.updateDate);
 		tv_isDead.setText(isDead);
-		for (int i=0; i<domain.a.size(); i++)
-			tv_a.setText(tv_a.getText()+"\n"+domain.a.get(i));
-		for (int i=0; i<domain.tXT.size(); i++)
-			tv_txt.setText(tv_txt.getText()+"\n"+domain.tXT.get(i));
+		for (int i = 0; i < domain.a.size(); i++)
+			tv_a.setText(tv_a.getText() + "\n" + domain.a.get(i));
+		for (int i = 0; i < domain.tXT.size(); i++)
+			tv_txt.setText(tv_txt.getText() + "\n" + domain.tXT.get(i));
 		tv_country.setText(domain.country);
-		for (int i=0; i<domain.a.size(); i++)
-			tv_ns.setText(tv_ns.getText()+"\n"+domain.nS.get(i));
+		for (int i = 0; i < domain.a.size(); i++)
+			tv_ns.setText(tv_ns.getText() + "\n" + domain.nS.get(i));
 		tv_resolvable.setText(resolvable);
 	}
 }
